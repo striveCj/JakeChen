b0bf792a7d01fab88aac1d6646cc4728dece7b9b
JackChen <530216775@qq.com>
2019-01-06 22:39:57 +0800

    分离控制器方法

---
+++ MyRepository/MyRepository.DAL/User/UserService.cs
@@ -0,0 +1,14 @@
+﻿using MyRepository.Data.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepository.DAL
+{
+    public class UserService:IUserService
+    {
+
+    }
+}
