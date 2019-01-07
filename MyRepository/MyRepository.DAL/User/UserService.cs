7cb127cc8d4a93eefa241f5e2883501b02ef5b11
JackChen <530216775@qq.com>
2019-01-07 20:54:49 +0800

    增加方法

--- MyRepository/MyRepository.DAL/User/UserService.cs
+++ MyRepository/MyRepository.DAL/User/UserService.cs
@@ -1,14 +1,31 @@
-﻿using MyRepository.Data.User;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyRepository.Data.User;
+using MyRepository.Models.Base;
+using MyRepository.Models.Dto;
 
-namespace MyRepository.DAL
+namespace MyRepository.DAL.User
 {
-    public class UserService:IUserService
+    public class UserService : IUserService
     {
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public List<UserDto> GetList(DataTableParameter param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyRepository.Models.Dto.UserDto GetModel(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(MyRepository.Models.Dto.UserDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MyRepository/MyRepository.Data/User/IUserService.cs
+++ MyRepository/MyRepository.Data/User/IUserService.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyRepository.Models.Base;
+using MyRepository.Models.Dto;
 
 namespace MyRepository.Data.User
 {
     public interface IUserService
     {
-
+        void Update(UserDto dto);
+        UserDto GetModel(Guid id);
+        void Delete(Guid id);
+        List<UserDto> GetList(DataTableParameter param);
     }
 }
