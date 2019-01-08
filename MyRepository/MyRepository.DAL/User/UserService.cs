944a0e45e4bf806fb6116e70ee0c3dfa74542c77
JackChen <530216775@qq.com>
2019-01-08 22:21:22 +0800

    方法转移

--- MyRepository/MyRepository.DAL/User/UserService.cs
+++ MyRepository/MyRepository.DAL/User/UserService.cs
@@ -1,31 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using MyRepository.Data.Core;
 using MyRepository.Data.User;
 using MyRepository.Models.Base;
 using MyRepository.Models.Dto;
+using MyRepository.Models.Entities;
 
 namespace MyRepository.DAL.User
 {
     public class UserService : IUserService
     {
+        private readonly UnitOfWork _unitOfWork = new UnitOfWork();
+        private readonly Repository<UserEntity> _repository;
+
+        public UserService()
+        {
+            this._repository = _unitOfWork.Repository<UserEntity>(); 
+        }
+        public UserService(Repository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            if (id == default(Guid))
+            {
+                throw new ArgumentException("id不能是Guid默认值", nameof(id));
+            }
+            UserEntity userEntity = _repository.GetById(id);
+            userEntity.IsDelete = true;
+            userEntity.ModifyDateTime = DateTime.Now;
+            _repository.Update(userEntity);
         }
 
         public List<UserDto> GetList(DataTableParameter param)
         {
-            throw new NotImplementedException();
+            List<UserEntity> entityList = new List<UserEntity>();
+            List<UserDto> dtoList = new List<UserDto>();
+            int listCount = _repository.Table.Count();
+
+            if (string.IsNullOrEmpty(param.sSearch))
+            {
+                entityList = _repository.Table.OrderBy(m => m.CreateDateTime).Skip((param.iDisplayStart / param.iDisplayLength) * param.iDisplayLength).Take(param.iDisplayLength).ToList();
+            }
+            else
+            {
+                entityList = _repository.Table.Where(item => item.UserName.Contains(param.sSearch)).OrderBy(m => m.CreateDateTime).Skip((param.iDisplayStart / param.iDisplayLength) * param.iDisplayLength).Take(param.iDisplayLength).ToList();
+            }
+
+            foreach (var item in entityList)
+            {
+                UserDto dto = new UserDto()
+                {
+                    Id = item.Id,
+                    UserName = item.UserName,
+                    Password = item.Password,
+                    Email = item.Email
+                };
+                dtoList.Add(dto);
+            }
+
+            return dtoList;
         }
 
-        public MyRepository.Models.Dto.UserDto GetModel(Guid id)
+        public UserDto GetModel(Guid id)
         {
-            throw new NotImplementedException();
+            UserEntity userEntity = _repository.GetById(id);
+            UserDto userDto = new UserDto()
+            {
+                Id = userEntity.Id,
+                UserName = userEntity.UserName,
+                Password = userEntity.Password,
+                Email = userEntity.Email
+            };
+            return userDto;
         }
 
-        public void Update(MyRepository.Models.Dto.UserDto dto)
+        public void Update(UserDto dto)
         {
-            throw new NotImplementedException();
+            UserEntity userEntity = _repository.GetById(dto.Id);
+            userEntity.UserName = dto.UserName;
+            userEntity.Email = dto.Email;
+            userEntity.Password = dto.Password;
+            _repository.Update(userEntity);
         }
     }
 }
