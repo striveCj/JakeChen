c37b9ca5d11bc29f8a1d70f4fd913e7f670987da
CHENJIE-PC\Administrator <530216775@qq.com>
2019-01-14 12:57:58 +0800

    增加泛型仓储接口，考虑分离仓储实现

---
+++ MyRepository/MyRepository.Data/Core/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyRepository.Models.Base;
+
+namespace MyRepository.Data.Core
+{
+    public interface IRepository<T> where T :BaseEntity
+    {
+        T GetById(Guid id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
